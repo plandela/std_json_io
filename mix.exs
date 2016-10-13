658a490ae36a5728356d4b3a33299922c44c0d1b
Simon Jesenko <simon.jesenko@gmail.com>
2016-10-13 10:09:16 +0200

    Add missing function

--- mix.exs
+++ mix.exs
@@ -44,4 +44,13 @@ defmodule StdJsonIo.Mixfile do
       {:fs, "~> 0.9.1"},
     ]
   end
+
+  defp package do
+    [
+      maintainers: @maintainers,
+      licenses: ["MIT"],
+      links: %{github: @url},
+      files: ~w(lib) ++ ~w(CHANGELOG.md LICENSE mix.exs README.md)
+    ]
+  end
 end
